5dcf202d746a1e0780563c454907d6705332d3f8
HowieCong <howiecong@163.com>
2025-08-24 16:58:42 +0800

    study 03_antv_basic_bar_tutorial.md

--- learn-antv-demo/src/main.tsx
+++ learn-antv-demo/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import BasicBarDemo from './BasicBarDemo.tsx';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <nav style={{ padding: 16, background: '#f5f5f5' }}>
+        <Link to="/" style={{ marginRight: 16 }}>Home首页</Link>
+        <Link to="/bar-demo">BarDemo柱状图实战</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/bar-demo" element={<BasicBarDemo />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
